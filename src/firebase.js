4816cedb6dd9884752c2244f5351ebc47444e704
bohyeon <shannon_@hotmail.co.kr>
2021-09-08 10:29:33 +0900

    📦 eslint prettier 설정 추가

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBIYVNLdYKiEO1kMY8spVrmPJnufoCrQcc',
+    authDomain: 'seriousmeme-eab4a.firebaseapp.com',
+    projectId: 'seriousmeme-eab4a',
+    storageBucket: 'seriousmeme-eab4a.appspot.com',
+    messagingSenderId: '885220602393',
+    appId: '1:885220602393:web:c80688c409dfeb2a0fb088',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
