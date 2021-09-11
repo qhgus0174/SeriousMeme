5489bd0dccef8133e46bee7d91650d8cece14004
bohyeon <shannon_@hotmail.co.kr>
2021-09-11 12:51:53 +0900

    🔥 라우터 필요없는 코드 삭제
    - 로그인 페이지를 라우터에서 컴포넌트로 변경하면서 라우터 파라미터 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import Home from 'routes/Home';
-import Auth from 'routes/Auth';
 import Main from 'routes/Main';
 
 import Header from '~components/Layout/Header';
 
-const AppRouter = ({ isLoggedIn }: { isLoggedIn?: boolean }) => (
+const AppRouter = () => (
     <Router>
         <Header />
         <Route exact path="/" component={Main} />
-        {/* <Route exact path="/" component={isLoggedIn ? Home : Auth} /> */}
     </Router>
 );
 
