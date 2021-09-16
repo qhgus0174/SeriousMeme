e7402ba9a3277191792fff1764aac405ae0bf49c
bohyeon <shannon_@hotmail.co.kr>
2021-09-16 19:50:56 +0900

    🎨 AuthContext 수정
    - useEffect 삭제

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,26 +1,30 @@
-import { authService } from 'fbInstance';
+import { auth } from 'fbInstance';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import React, { createContext, useEffect, useState } from 'react';
 
 interface IAuth {
-    init: boolean;
-    authUser: User | null;
+    state: {
+        init: boolean;
+        authUser: User | null;
+    };
 }
 
-export const AuthContext = createContext<IAuth>({ init: false, authUser: null });
+export const AuthContext = createContext<IAuth>({ state: { init: false, authUser: null } });
 
 const AuthProvider: React.FC = props => {
     const [authUser, setAuthUser] = useState<User | null>(null);
     const [init, setInit] = useState<boolean>(false);
 
-    useEffect(() => {
-        onAuthStateChanged(authService, user => {
-            user ? setAuthUser(user) : setAuthUser(null);
-            setInit(true);
-        });
-    }, []);
+    onAuthStateChanged(auth, (user: User | null) => {
+        user ? setAuthUser(user) : setAuthUser(null);
+        setInit(true);
+    });
 
-    return <AuthContext.Provider value={{ init: init, authUser: authUser }}>{props.children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider value={{ state: { init: init, authUser: authUser } }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
