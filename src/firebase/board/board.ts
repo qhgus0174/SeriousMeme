85e98d39c5fe7f6dabca71390db7c78589651ab6
bohyeon <shannon_@hotmail.co.kr>
2021-09-18 22:18:33 +0900

    🚚 firebase instance 구조 변경

---
+++ src/firebase/board/board.ts
@@ -0,0 +1,32 @@
+import { db } from 'firebase/firebaseInstance';
+import {
+    collection,
+    addDoc as fsAddDoc,
+    query,
+    orderBy,
+    deleteDoc as fsDelDoc,
+    updateDoc as fsUpdateDoc,
+    doc as fsDoc,
+} from 'firebase/firestore';
+
+export interface IBoard {
+    docId: string;
+    content: string;
+    createUserId?: string | null;
+    createUserEmail?: string | null;
+    createAt?: number;
+}
+
+//게시물 - board collection
+export const boardCollection = collection(db, 'board');
+export const queryBoardCollection = query(collection(db, 'board'), orderBy('createAt', 'asc'));
+
+const doc = (docId: string) => fsDoc(boardCollection, docId);
+
+export const addDoc = async (data: Pick<IBoard, 'content' | 'createUserId' | 'createUserEmail' | 'createAt'>) =>
+    await fsAddDoc(boardCollection, Object.assign({}, data, { createAt: Date.now() }));
+
+export const updateDoc = async ({ docId, content }: Pick<IBoard, 'docId' | 'content'>) =>
+    await fsUpdateDoc(doc(docId), { content: content, createAt: Date.now() });
+
+export const deleteDoc = async ({ docId }: Pick<IBoard, 'docId'>) => await fsDelDoc(doc(docId));
---
+++ src/firebase/firebaseInstance.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'; //Auth 사용
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig); //firebase initialize
+
+export const auth = getAuth();
+//database init
+export const db = getFirestore();
