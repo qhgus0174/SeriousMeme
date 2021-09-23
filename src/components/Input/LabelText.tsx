ce3b6157d9d4b2a31352bcd76b64ae1aa9dd748b
bohyeon <shannon_@hotmail.co.kr>
2021-09-24 03:39:03 +0900

    ✨ infield label input 추가

---
+++ src/components/Input/LabelText.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { ITextProps } from './TextBox';
+
+interface ILabelTextProps extends ITextProps {
+    label: string;
+}
+
+const LabelText = ({ label, ...rest }: ILabelTextProps) => {
+    return (
+        <TextFormDiv>
+            <Label>
+                <Span>{label}</Span>
+                <Text type="text" {...rest} />
+            </Label>
+        </TextFormDiv>
+    );
+};
+
+const TextFormDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    margin: 1em;
+    width: 100%;
+`;
+
+const Span = styled.span`
+    margin-bottom: 5px;
+    color: ${props => props.theme.colors.main};
+`;
+const Label = styled.label`
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    padding: 0.4em 0.5em 0.5em 0.5em;
+    border: 1px solid ${props => props.theme.colors.white};
+    border-radius: 3px;
+    outline-width: 3px;
+    outline-offset: -2px;
+    background-color: ${props => props.theme.colors.mainBackground};
+    box-shadow: inset 0 0 2px #999999;
+
+    &:focus-within {
+        outline-color: ${props => props.theme.colors.main};
+        outline-style: auto;
+    }
+
+    &:focus-within ${Span} {
+        color: ${props => props.theme.colors.main};
+    }
+    font-size: 15px;
+    width: 100%;
+`;
+
+const Text = styled.input`
+    border: none;
+    outline: none;
+    background: none;
+    width: 100%;
+    font-size: 14px;
+`;
+export default LabelText;
