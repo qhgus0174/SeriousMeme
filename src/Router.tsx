40603cf915f3f4ccc51a528957801a37f43ced17
bohyeon <shannon_@hotmail.co.kr>
2021-12-20 17:41:53 +0900

    🎨 seo 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ import { media } from '~styles/device';
 import Main from '~routes/Main';
 import List from '~routes/List';
 import Profile from '~routes/Profile';
+import { Helmet } from 'react-helmet';
 
 const AppRouter = () => {
     const {
@@ -26,6 +27,19 @@ const AppRouter = () => {
         <BrowserRouter basename="SeriousMeme">
             <Container>
                 <ModalContext>
+                    <Helmet>
+                        <title>인간극장 짤 생성기</title>
+                        <meta name="description" content="웹 페이지에서 편하게 인간극장 짤을 생성해보세요!" />
+                        <meta
+                            name="keywords"
+                            content="인간극장, 인간극장 짤, 인간극장 짤 생성기, 짤 생성기, 짤 만들기"
+                        />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:title" content="인간극장 짤 생성기" />
+                        <meta property="og:description" content="웹 페이지에서 편하게 인간극장 짤을 생성해보세요!" />
+                        <meta property="og:locale" content="ko_KR" />
+                        <meta property="og:url" content="https://qhgus0174.github.io/SeriousMeme/" />
+                    </Helmet>
                     <Sidebar />
                     <Route exact path="/" component={Main} />
                     <Route exact path="/list" component={List} />
